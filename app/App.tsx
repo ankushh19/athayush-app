dfbd18258e92dc6dfb93bd4b4177fb7b5883e839
Ankush <ak9108824@gmail.com>
2025-12-15 16:22:56 +0530

    added web-view for web-app

---
+++ app/App.tsx
@@ -0,0 +1,17 @@
+import { StatusBar } from "expo-status-bar"
+import { View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { WebView } from "react-native-webview"
+
+const ATHAYUSH_URL = "https://www.athayush.app/"
+
+export default function App() {
+    return (
+        <SafeAreaView>
+            <StatusBar style="dark" />
+            <View style={{ width: "100%", height: "100%" }}>
+                <WebView source={{uri: ATHAYUSH_URL}}  />
+            </View>
+        </SafeAreaView>
+    )
+}
