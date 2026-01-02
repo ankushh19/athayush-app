0b09c5e78650aff683552a1dab72b424e21d33f9
Ankush <ak9108824@gmail.com>
2026-01-02 14:24:34 +0530

    changed status bar color and added splash image

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,22 @@
-import { StatusBar } from "expo-status-bar"
-import { View } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
-import { WebView } from "react-native-webview"
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { WebView } from "react-native-webview";
 
-const ATHAYUSH_URL = "https://www.athayush.app/"
+const ATHAYUSH_URL = "https://www.athayush.app/";
 
 export default function HomeScreen() {
-    return (
-        <SafeAreaView>
-            <StatusBar style="dark" />
-            <View style={{ width: "100%", height: "100%" }}>
-                <WebView source={{uri: ATHAYUSH_URL}}  />
-            </View>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#2f5073" }}>
+      <StatusBar
+        style="light"            
+        backgroundColor="#2f5073"
+        animated={false}
+      />
+
+      <View style={{ flex: 1 }}>
+        <WebView source={{ uri: ATHAYUSH_URL }} />
+      </View>
+    </SafeAreaView>
+  );
 }
